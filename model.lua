023a001c22490fd59153b1daaee5d90f4b22b8ab
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-11 11:12:07 +0200

    can read from disk

--- model.lua
+++ model.lua
@@ -4,10 +4,45 @@ local res = {
 }
 res.data ={}
 function res.init()
-    print('should read data from disk and store in mem')
-    -- check if file 
+    print('Initializing data from disk...')
+    local path = 'data.csv'
+    local file, err
+    
+    -- Try to open file for reading first
+    file, err = io.open(path, "rb")
+    
+    -- If file doesn't exist, create it
+    if not file then
+        print('File not found, creating new file: ' .. path)
+        file, err = io.open(path, "w")
+        if not file then
+            print('Error creating file: ' .. err)
+            return false
+        end
+        -- Write initial content if needed
+        file:write("")
+        file:close()
+        
+        -- Reopen for reading
+        file, err = io.open(path, "rb")
+        if not file then
+            print('Error opening new file: ' .. err)
+            return false
+        end
+    end
 
+    -- Read file content
+    local content = file:read("*a")
+    file:close()
+    
+    -- Store and verify data
+    res.data = content or ""
+    print('Data loaded successfully. Length: ' .. #res.data .. ' bytes')
+    
+    return true
 end
+
+
 
 function res.write()
     print('should write to disk')
