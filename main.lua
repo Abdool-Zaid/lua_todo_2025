8d32b6c86f6538bca45ae925d4cde1561460955f
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-12 10:37:11 +0200

    cleaning

--- main.lua
+++ main.lua
@@ -1,11 +1,4 @@
--- local model = require 'model'
 local view = require 'view'
--- local controller = require 'controller'
-
--- testing modules
--- print(model.test) 
--- print(view.test)
--- print(controller.test)
 
 view.init()
 -- todo , add help option
